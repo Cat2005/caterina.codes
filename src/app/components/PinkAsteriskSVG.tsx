fd9a3fa4b626e30c2a6cafac8299ca2fa645e649
cmammola2005 <cmammola2005@gmail.com>
2025-05-16 16:21:38 +0100

    tiny fix

--- src/app/components/PinkAsteriskSVG.tsx
+++ src/app/components/PinkAsteriskSVG.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const PinkAsteriskSVG = () => {
   return (
-    <svg width="14" height="15" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="17" height="17" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M8.02148 2.22046C8.02148 4.93653 7.77808 7.79098 8.26246 10.4551C8.4198 11.3204 8.25638 12.4809 8.64115 13.2505" stroke="#CA0079" stroke-width="3" stroke-linecap="round"/>
 <path d="M2.19666 7.92145C5.57255 7.92145 9.03083 7.67358 12.3591 7.67358" stroke="#CA0079" stroke-width="3" stroke-linecap="round"/>
 <path d="M3.43604 3.95557C4.18798 3.95557 4.56184 4.00498 5.17109 4.47884C5.75856 4.93576 6.281 5.50178 6.87861 5.93849C7.58156 6.45218 8.2811 7.4036 8.88907 8.04534C10.0453 9.26582 11.3003 10.4565 12.4831 11.6394" stroke="#CA0079" stroke-width="3" stroke-linecap="round"/>
