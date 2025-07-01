18e54011bd0ba90cbc641bfccf42e854d9560237
Caterina <cmammola2005@gmail.com>
2025-07-01 10:45:43 +0100

    updated w final degree :D

--- src/app/Experience.tsx
+++ src/app/Experience.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import ExperienceSlide from './components/ExperienceSlide';
-import Divider from '@/app/svgs/divider.svg';
-import Image from 'next/image';
-import DividerSVG from '@/app/components/DividerSVG';
+import React from "react";
+import ExperienceSlide from "./components/ExperienceSlide";
+import Divider from "@/app/svgs/divider.svg";
+import Image from "next/image";
+import DividerSVG from "@/app/components/DividerSVG";
 
 const Experience = () => {
   return (
@@ -10,31 +10,32 @@ const Experience = () => {
       <div className="w-[85%] sm:w-[50%] md:w-2/5 xl:w-[35%] mx-auto py-[10vh]">
         <div className="">
           <h1 className="text-5xl text-[#CA0079] font-newsreader mb-10 w-1/2">
-            professional experience</h1>
+            professional experience
+          </h1>
         </div>
         <div className="flex flex-col gap-3">
-          <ExperienceSlide 
+          <ExperienceSlide
             image="/posts/spotify-logo.png"
             title="Software Engineer Intern"
             company="Spotify"
             description="Full-stack development on the Spotify for Authors platform in Audiobooks team."
             duration="Summer 2025"
           />
-          <ExperienceSlide 
+          <ExperienceSlide
             image="/posts/gs-logo.png"
             title="Software Engineer Intern"
             company="Goldman Sachs"
             description="Worked on the Regulatory Engineering team, using Prometheus, Grafana, Java, PromQL."
             duration="Summer 2024"
           />
-          <ExperienceSlide 
+          <ExperienceSlide
             image="/posts/compsoc-logo.png"
             title="Tech secretary"
             company="CompSoc"
             description="Built comp-soc.com, managed Infball event website, maintained CompSoc's internal servers."
             duration="2024-2025"
           />
-          <ExperienceSlide 
+          <ExperienceSlide
             image="/posts/compsoc-logo.png"
             title="Sponsorship Coordinator"
             company="CompSoc"
@@ -44,19 +45,20 @@ const Experience = () => {
           <div className="w-full flex justify-center mt-5 mb-5">
             <DividerSVG />
           </div>
-          <ExperienceSlide 
+          <ExperienceSlide
+            isDegree={true}
             image="/posts/edi-logo.png"
             title="BSc Computer Science and AI"
             company="University of Edinburgh"
-            description="First Class (81%) \n 
-            Machine Learning Practical, Natural Language Understanding+, Computer Graphics, Reinforcement Learning, Foundations of Natural Language Processing."
+            degree="First Class, <pink> Ranked 1st in Programme </pink>"
+            description="
+            Relevant modules: Machine Learning Practical, NLP, Computer Graphics, Reinforcement Learning"
             duration="2021-2025"
           />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Experience;
-
--- src/app/components/ExperienceSlide.tsx
+++ src/app/components/ExperienceSlide.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
 interface ExperienceSlideProps {
   image: string;
@@ -7,49 +7,93 @@ interface ExperienceSlideProps {
   company: string;
   description?: string;
   duration: string;
-
+  isDegree?: boolean;
+  degree?: string;
 }
 
-const ExperienceSlide: React.FC<ExperienceSlideProps> = ({ title, company, description, duration, image }) => {
+const ExperienceSlide: React.FC<ExperienceSlideProps> = ({
+  title,
+  company,
+  description,
+  duration,
+  image,
+  isDegree,
+  degree,
+}) => {
   const [imageLoading, setImageLoading] = React.useState(true);
 
   return (
-    <div className={`p-4 rounded-lg border border-[#252525] hover:border-[#c02e7e]/30 backdrop-blur-sm bg-[#979797]/10  shadow-lg`}>
-       <div className='flex flex-row w-full items-center'>
-        <div className='w-[15%] mr-4 relative'>
-            {imageLoading && (
-              <div className="absolute inset-0 flex items-center justify-center">
-                <div className="animate-spin rounded-full h-8 w-8 border-4 border-solid border-[#c02e7e] border-t-transparent"></div>
-              </div>
-            )}
-            <Image 
-              src={image} 
-              alt={company} 
-              width={100} 
-              height={100} 
-              onLoadingComplete={() => setImageLoading(false)}
-              className={imageLoading ? 'opacity-0' : 'opacity-100 transition-opacity duration-300'}
-            />
-        </div>
-
-        <div className='flex flex-col w-full'>
-          <div className='flex flex-row justify-between w-full items-center'>
-            <p className={`text-sm mb-1 text-white`} style={{fontFamily: 'Avant-Garde-Medium'}}>{company}</p>
-            <p className='sm:text-sm mb-1.5 text-xs text-[#dddddd]'>{duration}</p>
+    <div
+      className={`p-4 rounded-lg border border-[#252525] hover:border-[#c02e7e]/30 backdrop-blur-sm bg-[#979797]/10  shadow-lg`}
+    >
+      <div className="flex flex-row w-full items-center">
+        <div className="w-[15%] mr-4 relative">
+          {imageLoading && (
+            <div className="absolute inset-0 flex items-center justify-center">
+              <div className="animate-spin rounded-full h-8 w-8 border-4 border-solid border-[#c02e7e] border-t-transparent"></div>
             </div>
-            <p className='text-xs md:text-sm text-[#ebebeb] mt-[-5px] mb-[0.5] font-avantGardeMedium'>{title}</p>
-            <p className={`text-xs ${!title ? 'mt-2' : ''} text-[#dddddd]`}>{description?.split('\\n').map((line, i) => (
-              <React.Fragment key={i}>
-                {line}
-                {i < description?.split('\\n').length - 1 && <br />}
-              </React.Fragment>
-            ))}</p>
+          )}
+          <Image
+            src={image}
+            alt={company}
+            width={100}
+            height={100}
+            onLoadingComplete={() => setImageLoading(false)}
+            className={
+              imageLoading
+                ? "opacity-0"
+                : "opacity-100 transition-opacity duration-300"
+            }
+          />
         </div>
-       </div>
 
+        <div className="flex flex-col w-full">
+          <div className="flex flex-row justify-between w-full items-center">
+            <p
+              className={`text-sm mb-1 text-white`}
+              style={{ fontFamily: "Avant-Garde-Medium" }}
+            >
+              {company}
+            </p>
+            <p className="sm:text-sm mb-1.5 text-xs text-[#dddddd]">
+              {duration}
+            </p>
+          </div>
+          <p className="text-xs md:text-sm text-[#ebebeb] mt-[-5px] mb-[0.5] font-avantGardeMedium">
+            {title}
+          </p>
+          {isDegree && (
+            <p className="text-xs text-[#ebebeb] font-medium mt-[1px] mb-1 font-avantGardeMedium">
+              {degree?.split(/(<pink>.*?<\/pink>)/g).map((part, i) => {
+                if (part.startsWith("<pink>") && part.endsWith("</pink>")) {
+                  const content = part.slice(6, -7); // Remove <pink> and </pink>
+                  return (
+                    <span key={i} className="text-[#CA0079]">
+                      {content}
+                    </span>
+                  );
+                }
+                return part ? <span key={i}>{part}</span> : null;
+              })}
+            </p>
+          )}
+          <p className={`text-xs ${!title ? "mt-2" : ""} text-[#dddddd]`}>
+            {description?.split(/(<pink>.*?<\/pink>)/g).map((part, i) => {
+              if (part.startsWith("<pink>") && part.endsWith("</pink>")) {
+                const content = part.slice(6, -7); // Remove <pink> and </pink>
+                return (
+                  <span key={i} className="text-[#CA0079]">
+                    {content}
+                  </span>
+                );
+              }
+              return part ? <span key={i}>{part}</span> : null;
+            })}
+          </p>
+        </div>
+      </div>
     </div>
-
-  )
-}
+  );
+};
 
 export default ExperienceSlide;
